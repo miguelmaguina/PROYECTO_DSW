5771d94635f062702739ec52340bc752dba26bcd
RenzoTacuri <renzo.tacuri@unmsm.edu.pe>
2023-01-21 09:55:57 -0500

    Collage para las innovaciones

---
+++ Tarea2/scriptB2.js
@@ -0,0 +1,11 @@
+const fulImgBox=document.getElementById("fulImgBox");
+fulImgBox=document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display="flex";
+    fulImg.src=reference;
+}
+
+function closeImg(){
+    fulImgBox.style.display="none";
+}
