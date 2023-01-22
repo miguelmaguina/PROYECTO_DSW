9cb9d676a37376173f29b5cc68132e771dcc0896
LUIS <luis.mayta@unmsm.edu.pe>
2023-01-22 01:11:06 -0500

    avance 15.0

--- Tarea2/scriptB1.js
+++ Tarea2/scriptB1.js
@@ -17,3 +17,25 @@ window.sr=ScrollReveal();
         origin: 'top',
         distance: '-100px'
     });
+
+    (function(){
+        const titleQuestions = [...document.querySelectorAll('.desplegable-title')];
+        console.log(titleQuestions)
+    
+        titleQuestions.forEach(question =>{
+            question.addEventListener('click', ()=>{
+                let height = 0;
+                let answer = question.nextElementSibling;
+                let addPadding = question.parentElement.parentElement;
+    
+                addPadding.classList.toggle('desplegable-padding--add');
+                question.children[0].classList.toggle('desplegable-arrow--rotate');
+    
+                if(answer.clientHeight === 0){
+                    height = answer.scrollHeight;
+                }
+    
+                answer.style.height = `${height}px`;
+            });
+        });
+    })();
