c804991f7a33a5513905ff7c7dd176b652db2937
RenzoTacuri <renzo.tacuri@unmsm.edu.pe>
2023-02-27 09:19:11 -0500

    URL_Otros comentado dado q no todos lo poseen

--- G1-PROYECTO-DSW/Clases/Emprendimiento.php
+++ G1-PROYECTO-DSW/Clases/Emprendimiento.php
@@ -1,8 +1,166 @@
 <?php
 
-class Emprendimiento {
-    
+class Emprendimiento
+{
+    private $ID_Emprendimiento;
+    private $Nombre;
+    private $Usuario;
+    private $Email;
+    private $Celular;
+    private $Contrasena;
+    private $Departamento;
+    private $Direccion;
+    private $Logo;
+    private $Fecha_Creacion;
+    private $URL_WEB;
+    private $URL_Facebook;
+    private $URL_Instagram;
+    //private $URL_Otros;
 
+    //Constructor vacío
+    public function Emprendimiento(){}
+
+    //Getters
+    public function getID_Emprendimiento()
+    {
+        return $this->ID_Emprendimiento;
+    }
+
+    public function getNombre()
+    {
+        return $this->Nombre;
+    }
+
+    public function getUsuario()
+    {
+        return $this->Usuario;
+    }
+
+    public function getEmail()
+    {
+        return $this->Email;
+    }
+
+    public function getCelular()
+    {
+        return $this->Celular;
+    }
+
+    public function getContrasena()
+    {
+        return $this->Contrasena;
+    }
+
+    public function getDepartamento()
+    {
+        return $this->Departamento;
+    }
+
+    public function getDireccion()
+    {
+        return $this->Direccion;
+    }
+
+    public function getLogo()
+    {
+        return $this->Logo;
+    }
+
+    public function getFecha_Creacion()
+    {
+        return $this->Fecha_Creacion;
+    }
+
+    public function getURL_WEB()
+    {
+        return $this->URL_WEB;
+    }
+
+    public function getURL_Facebook()
+    {
+        return $this->URL_Facebook;
+    }
+
+    public function getURL_Instagram()
+    {
+        return $this->URL_Instagram;
+    }
+
+    /*public function getURL_Otros()
+    {
+        return $this->URL_Otros;
+    }*/
+
+    //Setters
+    public function setID_Emprendimiento($ID_Emprendimiento)
+    {
+        $this->ID_Emprendimiento=$ID_Emprendimiento;
+    }
+
+    public function setNombre($Nombre)
+    {
+        $this->Nombre=$Nombre;
+    }
+
+    public function setUsuario($Usuario)
+    {
+        $this->Usuario=$Usuario;
+    }
+
+    public function setEmail($Email)
+    {
+        $this->Email=$Email;
+    }
+
+    public function setCelular($Celular)
+    {
+        $this->Celular=$Celular;
+    }
+
+    public function setContrasena($Contrasena)
+    {
+        $this->Contrasena=$Contrasena;
+    }
+
+    public function setDepartamento($Departamento)
+    {
+        $this->Departamento=$Departamento;
+    }
+
+    public function setDireccion($Direccion)
+    {
+        $this->Direccion=$Direccion;
+    }
+
+    public function setLogo($Logo)
+    {
+        $this->Logo=$Logo;
+    }
+
+    public function setFecha_Creacion($Fecha_Creacion)
+    {
+        $this->Fecha_Creacion=$Fecha_Creacion;
+    }
+
+    public function setURL_WEB($URL_WEB)
+    {
+        $this->URL_WEB=$URL_WEB;
+    }
+
+    public function setURL_Facebook($URL_Facebook)
+    {
+        $this->URL_Facebook=$URL_Facebook;
+    }
+
+    public function setURL_Instagram($URL_Instagram)
+    {
+        $this->URL_Instagram=$URL_Instagram;
+    }
+
+    /*public function setURL_Otros($URL_Otros)
+    {
+        $this->URL_Otros=$URL_Otros;
+    }*/
 }
 
 ?>
--- G1-PROYECTO-DSW/Clases/Producto.php
+++ G1-PROYECTO-DSW/Clases/Producto.php
@@ -1,8 +1,156 @@
 <?php
 
-class Producto {
-    
+class Producto
+{
+    //Atributos
+    private $ID_Producto;
+    private $Nombre;
+    private $Precio;
+    private $Descripcion;
+    private $ID_Categoria;
+    private $ID_Subcategoria;
+    private $Descuento;
+    private $Fecha;
+    private $ID_Emprendimiento;
+    private $Disponibilidad;
+    private $Foto_Secundaria1;
+    private $Foto_Secundaria2;
+    private $Foto_Secundaria3;
 
+    //Constructor vacío
+    public function Producto(){}
+
+    //Getters
+    public function ID_Producto()
+    {
+        return $this->ID_Producto;
+    }
+
+    public function getNombre()
+    {
+        return $this->Nombre;
+    }
+
+    public function getPrecio()
+    {
+        return $this->Precio;
+    }
+
+    public function getDescripcion()
+    {
+        return $this->Descripcion;
+    }
+
+    public function getID_Categoria()
+    {
+        return $this->ID_Categoria;
+    }
+
+    public function getID_Subcategoria()
+    {
+        return $this->ID_Subcategoria;
+    }
+
+    public function getDescuento()
+    {
+        return $this->Descuento;
+    }
+
+    public function getFecha()
+    {
+        return $this->Fecha;
+    }
+
+    public function getID_Emprendimiento()
+    {
+        return $this->ID_Emprendimiento;
+    }
+
+    public function getDisponibilidad()
+    {
+        return $this->Disponibilidad;
+    }
+
+    public function getFoto_Secundaria1()
+    {
+        return $this->Foto_Secundaria1;
+    }
+
+    public function getFoto_Secundaria2()
+    {
+        return $this->Foto_Secundaria2;
+    }
+
+    public function getFoto_Secundaria3()
+    {
+        return $this->Foto_Secundaria3;
+    }
+
+    //Setters
+    public function setID_Producto($ID_Producto)
+    {
+        $this->ID_Producto=$ID_Producto;
+    }
+
+    public function setNombre($Nombre)
+    {
+        $this->Nombre=$Nombre;
+    }
+
+    public function setPrecio($Precio)
+    {
+        $this->Precio=$Precio;
+    }
+
+    public function setDescripcion($Descripcion)
+    {
+        $this->Descripcion=$Descripcion;
+    }
+
+    public function setID_Categoria($ID_Categoria)
+    {
+        $this->ID_Categoria=$ID_Categoria;
+    }
+
+    public function setID_Subcategoria($ID_Subcategoria)
+    {
+        $this->ID_Subcategoria=$ID_Subcategoria;
+    }
+
+    public function setDescuento($Descuento)
+    {
+        $this->Descuento=$Descuento;
+    }
+
+    public function setFecha($Fecha)
+    {
+        $this->Fecha=$Fecha;
+    }
+
+    public function setID_Emprendimiento($ID_Emprendimiento)
+    {
+        $this->ID_Emprendimiento=$ID_Emprendimiento;
+    }
+
+    public function setDisponibilidad($Disponibilidad)
+    {
+        $this->Disponibilidad=$Disponibilidad;
+    }
+
+    public function setFoto_Secundaria1($Foto_Secundaria1)
+    {
+        $this->Foto_Secundaria1=$Foto_Secundaria1;
+    }
+
+    public function setFoto_Secundaria2($Foto_Secundaria2)
+    {
+        $this->Foto_Secundaria2=$Foto_Secundaria2;
+    }
+
+    public function setFoto_Secundaria3($Foto_Secundaria3)
+    {
+        $this->Foto_Secundaria3=$Foto_Secundaria3;
+    }
 }
 
 ?>
