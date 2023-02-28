c9ef34ff4c9d253b904b82f070ef2d9bd043e0eb
yanhuaman <yan.huaman@unmsm.edu.pe>
2023-02-28 17:08:40 -0500

    clase cliente y cliente DAO

--- G1-PROYECTO-DSW/Clases/Cliente.php
+++ G1-PROYECTO-DSW/Clases/Cliente.php
@@ -1,8 +1,135 @@
 <?php
 
 class Cliente {
-    
 
+    //Atributos
+    private $ID_Cliente;
+    private $Nombres;
+    private $Apellidos;
+    private $Email;
+    private $Celular;
+    private $Contrasena;
+    private $Departamento;
+    private $Usuario;
+    private $DNI;
+    private $Fecha_Creacion;
+    private $Foto_Perfil;
+
+    //Constructor vacío
+    public function Producto(){}
+
+    //Getters
+    public function getID_Cliente()
+    {
+        return $this->ID_Cliente;
+    }
+
+    public function getNombres()
+    {
+        return $this->Nombres;
+    }
+
+    public function getApellidos()
+    {
+        return $this->Apellidos;
+    }
+
+    public function getEmail()
+    {
+        return $this->Email;
+    }
+
+    public function getCelular()
+    {
+        return $this->Celular;
+    }
+
+    public function getContraseña()
+    {
+        return $this->Contrasena;
+    }
+
+    public function getDepartamento()
+    {
+        return $this->Departamento;
+    }
+
+    public function getUsuario()
+    {
+        return $this->Usuario;
+    }
+
+    public function getDNI()
+    {
+        return $this->DNI;
+    }
+
+    public function getFecha_Creacion()
+    {
+        return $this->Fecha_Creacion;
+    }
+
+    public function getFoto_Perfil()
+    {
+        return $this->Foto_Perfil;
+    }
+
+    //Setters
+    public function setID_Cliente($ID_Cliente)
+    {
+        $this->ID_Cliente=$ID_Cliente;
+    }
+
+    public function setNombres($Nombres)
+    {
+        $this->Nombres=$Nombres;
+    }
+
+    public function setApellidos($Apellidos)
+    {
+        $this->Apellidos=$Apellidos;
+    }
+
+    public function setEmail($Email)
+    {
+        $this->Email=$Email;
+    }
+
+    public function setCelular($Celular)
+    {
+        $this->Celular=$Celular;
+    }
+
+    public function setContrasena($Contrasena)
+    {
+        $this->Contrasena=$Contrasena;
+    }
+
+    public function setDepartamento($Departamento)
+    {
+        $this->Departamento=$Departamento;
+    }
+
+    public function setUsuario($Usuario)
+    {
+        $this->Usuario=$Usuario;
+    }
+
+    public function setDNI($DNI)
+    {
+        $this->DNI=$DNI;
+    }
+
+    public function setFecha_Creacion($Fecha_Creacion)
+    {
+        $this->Fecha_Creacion=$Fecha_Creacion;
+    }
+
+    public function setFoto_Perfil($Foto_Perfil)
+    {
+        $this->Foto_Perfil=$Foto_Perfil;
+    }
+   
 }
 
 ?>
--- G1-PROYECTO-DSW/DAO/ClienteDAO.php
+++ G1-PROYECTO-DSW/DAO/ClienteDAO.php
@@ -4,8 +4,180 @@ include "../Conexion/Conexion.php";
 include "../Clases/Cliente.php";
 
 class ClienteDAO {
-    
+    //Atributos
+    private $conexion;
+
+    //Constructor
+    public function __construct(){
+        $this->conexion = new Conexion();
+    }
+
+    //Destructor (Se ejecutará al final del script)
+    public function __destruct() {
+        $this->conexion->close();
+    }
+
+    //CRUD
+    public function listar(){
+        $clientes = array();
+        $query = "SELECT * FROM Cliente";
+
+        try{
+            $stmt = mysqli_prepare($this->conexion->getConexion(), $query);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $ID_Cliente, $Nombres, $Apellidos, $Email, $Celular, $Contrasena, $Departamento, $Usuario, $DNI, $Fecha_Creacion, $Foto_Perfil);
+
+            while (mysqli_stmt_fetch($stmt)) {
+                $cliente = new Cliente();
+                $cliente->setID_Cliente($ID_Cliente);
+                $cliente->setNombres($Nombres);
+                $cliente->setApellidos($Apellidos);
+                $cliente->setEmail($Email);
+                $cliente->setCelular($Celular);
+                $cliente->setContrasena($Contrasena);
+                $cliente->setDepartamento($Departamento);
+                $cliente->setUsuario($Usuario);
+                $cliente->setDNI($DNI);
+                $cliente->setFecha_Creacion($Fecha_Creacion);
+                $cliente->setFoto_Perfil($Foto_Perfil);
+                $clientes[] = $cliente;
+            }
+
+        } catch (Exception $e) {
+            echo "Error al listar clientes: " . $e->getMessage();
+            $clientes = null;
+        } finally{
+            if($stmt){
+                mysqli_stmt_close($stmt);
+            }
+        }
+        return $clientes;
+    }
+
+    /*
+    Nota: El primero parametro de la funcion 'bind_param' requiere que se especifique el tipo de elemento que se le pasa.
+        i: para variables de tipo entero
+        d: para variables de tipo punto flotante
+        s: para variables de tipo cadena (string)
+        b: para variables de tipo blob
+        ETC...
+    */
+
+    public function insert(Cliente $cliente){
+
+        $query = "INSERT INTO Cliente(Nombres, Apellidos, email, Celular,Contraseña,Departamento, Usuario, DNI, Fecha_Creacion, Foto_Perfil) VALUES (?,?,?,?,?,?,?,?,?,?)";
+
+        try{
+            $stmt = mysqli_prepare($this->conexion->getConexion(), $query);
+
+            //$ID_Cliente = $cliente->getID_Cliente(); //i
+            $Nombres = $cliente->getNombres(); //s
+            $Apellidos = $cliente->getApellidos(); //s
+            $Email = $cliente->getEmail(); //s
+            $Celular = $cliente->getCelular(); //s
+            $Contrasena = $cliente->getContrasena(); //s
+            $Departamento = $cliente->getDepartamento(); //s
+            $Usuario = $cliente->getUsuario(); //s
+            $DNI = $cliente->getDNI(); //s
+            $Fecha_Creacion = $cliente->getFecha_Creacion(); //s
+            $Foto_Perfil = $cliente->getFoto_Perfil(); //s
+
+            mysqli_stmt_bind_param($stmt, "ssssssssss", $Nombres, $Apellidos, $Email, $Celular, $Contrasena, $Departamento, $Usuario, $DNI, $Fecha_Creacion, $Foto_Perfil);
+            mysqli_stmt_execute($stmt);
+        
+        } catch (Exception $e) {
+            echo "Error al insertar cliente: " . $e->getMessage();
+        } finally{
+            if($stmt){
+                mysqli_stmt_close($stmt);
+            }
+        }
+    }
+
+    public function update(Cliente $cliente) {
+
+        $query = "UPDATE emprendimiento SET Nombres=?, Apellidos=?, Email=?, Celular=?, Contrasena=?, Departamento=?, Usuario=?, DNI=?, Fecha_Creacion=?, Foto_Perfil=? WHERE ID_Cliente=?";
+
+        try {
+            $stmt = mysqli_prepare($this->conexion->getConexion(), $query);
+
+            $Nombres = $cliente->getNombres(); //s
+            $Apellidos = $cliente->getApellidos(); //s
+            $Email = $cliente->getEmail(); //s
+            $Celular = $cliente->getCelular(); //s
+            $Contrasena = $cliente->getContrasena(); //s
+            $Departamento = $cliente->getDepartamento(); //s
+            $Usuario = $cliente->getUsuario(); //s
+            $DNI = $cliente->getDNI(); //s
+            $Fecha_Creacion = $cliente->getFecha_Creacion(); //s
+            $Foto_Perfil = $cliente->getFoto_Perfil(); //s
+            $ID_Cliente = $cliente->getID_Cliente(); //i
+
+            mysqli_stmt_bind_param($stmt, "sssssssssss", $Nombres, $Apellidos, $Email, $Celular, $Contrasena, $Departamento, $Usuario, $DNI, $Fecha_Creacion, $Foto_Perfil, $ID_Cliente);
+            mysqli_stmt_execute($stmt);
+
+        } catch (Exception $e) {
+            echo "Error al actualizar cliente: " . $e->getMessage();
+        } finally {
+            if ($stmt) {
+                mysqli_stmt_close($stmt);
+            }
+        }
+    }
+
+    public function delete($ID_Cliente) {
+        $query = "DELETE FROM Cliente WHERE ID_Cliente=?";
+        try{
+            $stmt = mysqli_prepare($this->conexion->getConexion(), $query);
+            mysqli_stmt_bind_param($stmt, "s", $ID_Cliente);
+            mysqli_stmt_execute($stmt);
+        } catch (Exception $e) {
+            echo "Error al eliminar cliente: " . $e->getMessage();
+        } finally{
+            if($stmt){
+                mysqli_stmt_close($stmt);
+            }
+        }
+    }
+
+    public function listarPorIdEmprendimiento($ID_Emprendimiento_Buscado){
+
+        $query = "SELECT * FROM Cliente WHERE ID_Cliente=?";
+
+        try{
+
+            $stmt = mysqli_prepare($this->conexion->getConexion(), $query);
+            mysqli_stmt_bind_param($stmt, "i", $ID_Cliente_Buscado);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $ID_Cliente, $Nombres, $Apellidos, $Email, $Celular, $Contrasena, $Departamento, $Usuario, $DNI, $Fecha_Creacion, $Foto_Perfil);
+            
+            if (mysqli_stmt_fetch($stmt)) {
+                $cliente = new cliente();
+                $ID_Cliente = $cliente->getID_Cliente(); //i
+                $Nombres = $cliente->getNombres(); //s
+                $Apellidos = $cliente->getApellidos(); //s
+                $Email = $cliente->getEmail(); //s
+                $Celular = $cliente->getCelular(); //s
+                $Contrasena = $cliente->getContrasena(); //s
+                $Departamento = $cliente->getDepartamento(); //s
+                $Usuario = $cliente->getUsuario(); //s
+                $DNI = $cliente->getDNI(); //s
+                $Fecha_Creacion = $cliente->getFecha_Creacion(); //s
+                $Foto_Perfil = $cliente->getFoto_Perfil(); //s
+            }
+
+        } catch (Exception $e) {
+            echo "Error al listar 1 cliente: " . $e->getMessage();
+            $cliente = null;
+        } finally{
+            if($stmt){
+                mysqli_stmt_close($stmt);
+            }
+        }
+        return $cliente;
+    }
 
 }
+?> 
 
-?>
+
