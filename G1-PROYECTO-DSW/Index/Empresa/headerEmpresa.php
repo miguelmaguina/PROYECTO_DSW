a6035885edeaa76f71daa1fa18a3e541c60c22f4
LUIS <luis.mayta@unmsm.edu.pe>
2023-02-27 21:27:33 -0500

    .

--- G1-PROYECTO-DSW/Index/Empresa/headerEmpresa.php
+++ G1-PROYECTO-DSW/Index/Empresa/headerEmpresa.php
@@ -1,10 +1,10 @@
 <?php
 
 ?>
-<nav class="navbar p-3 navbar-expand-md bg-white">
+<nav class="navbar p-3 navbar-expand-md">
         <div class="container-fluid col">
             <div class="d-flex align-items-center justify-content-between"> 
-                <a href="../index.php" class="logo d-flex align-items-center text-decoration-none"> <img src="../../Image/logo1.png" alt=""> <span class="d-none d-lg-block">Prueba</span> </a>
+                <a href="../index.php" class="logo d-flex align-items-center text-decoration-none"> <img src="../../Image/hallpa.png" alt=""> <span class="d-none d-lg-block">Hallpa</span> </a>
              </div>
              <div class="search-bar">
                 <form class="search-form d-flex align-items-center" method="POST" action="#"> <input type="text" name="query" placeholder="Buscar..." title="Escriba"> <button type="submit" title="Buscar"><i class="fa-solid fa-magnifying-glass"></i></button></form>
@@ -33,11 +33,11 @@
         <div class="container-fluid col ">
             <nav class="header-nav ms-auto position-absolute" style="right: 20px; top:16px;">
                 <ul class="d-flex align-items-center">
-                    <li class="nav-item d-block d-lg-none p-1">
+                    <li class="nav-item d-block d-xl-none p-1">
                         <a class="nav-link nav-icon search-bar-toggle " href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
                     </li>
                     <li class="nav-item nav-carrito car">
-                        <img src="../../Image/icono_money.png" alt="Profile" class="rounded-circle">
+                        <a href="#" class="nav-link nav-icon" ><i class="fa-solid fa-store"></i></a>
                     </li>
                    <li class="nav-item dropdown pe-3">
                       <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown"> <img src="../../Image/perfil-img.png" alt="Profile" class="rounded-circle"> <span class="d-none d-md-block dropdown-toggle ps-2">admin</span> </a>
