7c615b2855f244ee59b23eb5b8c222b6e2961458
LUIS <luis.mayta@unmsm.edu.pe>
2023-02-28 19:09:16 -0500

    mejorando carrusel

--- G1-PROYECTO-DSW/Index/index.php
+++ G1-PROYECTO-DSW/Index/index.php
@@ -14,6 +14,8 @@
     <link rel="stylesheet" href="../Estilos/styleIndex.css?v=<?php echo time(); ?>">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.css">
 
     <script type="text/javascript">
 		window.addEventListener("scroll", function(){
@@ -102,8 +104,7 @@
                 <p class="title-1 fs-2 h3" style="color: white;">Productos elaborados</p><p class="title-2 fs-1 fw-bold h2" style="color: #035941;">por comunidades nativas del Perú</p>
                 <p class="descripcion">Déjese sorprender por la belleza y autenticidad de los productos hechos a mano por las comunidades nativas peruanas. Desde ropa tejida con técnicas tradicionales hasta joyas y objetos de decoración únicos, cada uno de nuestros productos cuenta con una historia y una cultura detrás. Al comprar en nuestro e-commerce, no solo estará llevándose a casa un objeto hermoso, sino que también estará apoyando a las comunidades nativas y preservando sus tradiciones. ¡Descubra la belleza de las comunidades nativas peruanas hoy mismo!</p>
                 <div class="btn-group">
-                <a href="#" class="btn btn-izq btn-block mx-4 rounded fw-bold">Ir a la tienda virtual</a>
-                <a href="#" class="btn btn-der btn-block mx-4 rounded fw-bold">¡Regístrese!</a>
+                <a href="#" class="btn btn-der btn-block mx-4 rounded fw-bold">¡Iniciar!</a>
                 </div>
             </div>
         </div>
@@ -117,7 +118,7 @@
             <div class="row">
                 <div class="col-md-11 mx-auto text-center">
                     <h2 class="">Productos más vendidos</h2>
-                        <div id="carouselControls" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                        <!-- <div id="carouselControls" class="carousel carousel-dark slide" data-bs-ride="carousel">
                         <div class="carousel-inner carousel-p">
                             <div class="carousel-item active">
                             <div class="row">
@@ -200,7 +201,61 @@
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                             </button>
+                        </div> -->
+
+                        <div class="carousel">
+                        <div class="carousel__contenedor">
+                            <button aria-label="Anterior" class="carousel__anterior">
+                                <i class="fas fa-chevron-left"></i>
+                            </button>
+
+                            <div class="carousel__lista">
+                                
+                                <div class="card card-p mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Producto 3">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Manto artesanal para abrigarte</h5>
+                                        <p class="card-text">Descripción del producto 3.</p>
+                                        <span class="fs-4">S/300.00 <small>10% desc.</small> </span>
+                                    </div>
+                                </div>
+                                
+                                <div class="card card-p mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Producto 3">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Manto artesanal para abrigarte</h5>
+                                        <p class="card-text">Descripción del producto 3.</p>
+                                        <span class="fs-4">S/300.00 <small>10% desc.</small> </span>
+                                    </div>
+                                </div>
+
+                                <div class="card card-p mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Producto 3">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Manto artesanal para abrigarte</h5>
+                                        <p class="card-text">Descripción del producto 3.</p>
+                                        <span class="fs-4">S/300.00 <small>10% desc.</small> </span>
+                                    </div>
+                                </div>
+
+                                <div class="card card-p mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="Producto 3">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Manto artesanal para abrigarte</h5>
+                                        <p class="card-text">Descripción del producto 3.</p>
+                                        <span class="fs-4">S/300.00 <small>10% desc.</small> </span>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                            <button aria-label="Siguiente" class="carousel__siguiente">
+                                <i class="fas fa-chevron-right"></i>
+                            </button>
                         </div>
+
+                        <div role="tablist" class="carousel__indicadores"></div>
+                    </div>
 
                 </div>
             </div>
@@ -215,7 +270,7 @@
             <div class="row">
                 <div class="col-md-11 mx-auto text-center">
                     <h2 class="">Opiniones</h2>
-                    <div id="carouselExampleControls" class="carousel slide carousel-dark" data-bs-ride="carousel">
+                    <!-- <div id="carouselExampleControls" class="carousel slide carousel-dark" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                         <div class="row">
@@ -313,6 +368,80 @@
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                         </button>
+                    </div> -->
+
+                    <div class="carousel">
+                        <div class="carousel__contenedor">
+                            <button aria-label="Anterior" class="carousel__anterior anterior">
+                                <i class="fas fa-chevron-left"></i>
+                            </button>
+
+                            <div class="carousel__lista lista">
+                                
+
+                                <div class="card card-o mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top mx-auto d-block" alt="Producto 2">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Artesania</h5>
+                                        <p class="card-text">Descripción de la opinion</p>
+                                        <div class="contenido-img-span">
+                                            <img src="../Image/perfil-img.png" alt="Producto 1"><i>por Isabela Merced</i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card card-o mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top mx-auto d-block" alt="Producto 2">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Artesania</h5>
+                                        <p class="card-text">Descripción de la opinion</p>
+                                        <div class="contenido-img-span">
+                                            <img src="../Image/perfil-img.png" alt="Producto 1"><i>por Isabela Merced</i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card card-o mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top mx-auto d-block" alt="Producto 2">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Artesania</h5>
+                                        <p class="card-text">Descripción de la opinion</p>
+                                        <div class="contenido-img-span">
+                                            <img src="../Image/perfil-img.png" alt="Producto 1"><i>por Isabela Merced</i>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card card-o mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top mx-auto d-block" alt="Producto 2">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Artesania</h5>
+                                        <p class="card-text">Descripción de la opinion</p>
+                                        <div class="contenido-img-span">
+                                            <img src="../Image/perfil-img.png" alt="Producto 1"><i>por Isabela Merced</i>
+                                        </div>
+                                    </div>
+                                </div>
+                
+                                <div class="card card-o mb-3">
+                                    <img src="https://via.placeholder.com/300x200" class="card-img-top mx-auto d-block" alt="Producto 2">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Artesania</h5>
+                                        <p class="card-text">Descripción de la opinion</p>
+                                        <div class="contenido-img-span">
+                                            <img src="../Image/perfil-img.png" alt="Producto 1"><i>por Isabela Merced</i>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                            <button aria-label="Siguiente" class="carousel__siguiente siguiente">
+                                <i class="fas fa-chevron-right"></i>
+                            </button>
+                        </div>
+
+                        <div role="tablist" class="carousel__indicadores indicadores"></div>
                     </div>
                     
                 </div>
@@ -337,8 +466,12 @@
     </footer>
 
     <script src="../js/index.js"></script>
+    <script src="../js/carousel.js"></script>
     <!-- Scripts de Bootstrap 5 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.js"></script>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
     
 
 
