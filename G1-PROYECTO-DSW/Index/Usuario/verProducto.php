95151f0278724c5ad8b7710161a354c23d5db658
dianafran <diana.bardon@unmsm.edu.pe>
2023-03-02 16:20:28 -0500

    cambios para producto

--- G1-PROYECTO-DSW/Index/Usuario/verProducto.php
+++ G1-PROYECTO-DSW/Index/Usuario/verProducto.php
@@ -13,43 +13,38 @@ if($id == '' || $token == ''){
     $token_tmp = hash_hmac('sha1',$id,KEY_TOKEN);
 
     if($token == $token_tmp){
-        $sql = $con->prepare("SELECT count(id) FROM productos WHERE id=? AND activo=1");
+        $sql = $con->prepare("SELECT count(id) FROM productos WHERE ID_Producto=? AND Disponibilidad=1");
         $sql->execute([$id]);
 
         if($sql->fetchColumn() > 0){
-            $sql = $con->prepare("SELECT nombre, descripcion, precio, descuento FROM productos WHERE id=? AND activo=1 LIMIT 1");
+            $sql = $con->prepare("SELECT Nombre, Precio, Descripcion, Descuento, Foto_Secundaria1, Foto_Secundaria2, Foto_Secundaria3 FROM productos WHERE ID_Producto=? AND Disponibilidad=1 LIMIT 1");
             $sql->execute([$id]);
             $row = $sql->fetch(PDO::FETCH_ASSOC);
-            $nombre = $row['nombre'];
-            $descripcion = $row['descripcion'];
-            $precio = $row['precio'];
-            $descuento = $row['descuento'];
-            $precio_desc = $precio - (($precio*$descuento)/100);
-            $dir_images = '../../Image/Productos/'.$id.'/';
-            $dir_images = '../../Image/Productos/'.$id.'/';
-            $dir_images = '../../Image/Productos/'.$id.'/';
+            $Nombre = $row['Nombre'];
+            $Descripcion = $row['Descripcion'];
+            $precio_desc = $Precio - (($Precio*$Descuento)/100);
+            $Foto_Secundaria1 = '../../Image/Productos/Foto_Secundaria1/';
+            $Foto_Secundaria2 = '../../Image/Productos/Foto_Secundaria2/';
+            $Foto_Secundaria3 = '../../Image/Productos/Foto_Secundaria3/Foto_Secundaria3/';
 
 
-            $rutaImg = $dir_images . 'principal.jpg';
+            $rutaImg1 = $Foto_Secundaria1 . 'Foto_Secundaria1_'.$id.'.jpg';
+            $rutaImg2 = $Foto_Secundaria2 . 'Foto_Secundaria2_'.$id.'.jpg';
+            $rutaImg3 = $Foto_Secundaria3 . 'Foto_Secundaria3_'.$id.'.jpg';
 
-            if(!file_exists($rutaImg)){
-                $rutaImg = 'images/none.png';
+
+            if(!file_exists($rutaImg1)){
+              $rutaImg1 = 'Image/none.png';
             }
-
-            $images = array();
-            if(file_exists($dir_images)){
-                $dir = dir($dir_images);
-
-                while(($archivo = $dir->read()) != false){
-                    if($archivo != 'principal.jpg' && (strpos($archivo,'jpg') || strpos($archivo,'jpge'))){
-                        $imagenes[] = $dir_images . $archivo;
-                    }
-                }
-                $dir->close();
+            if(!file_exists($rutaImg2)){
+              $rutaImg2 = 'Image/none.png';
+            }
+            if(!file_exists($rutaImg1)){
+              $rutaImg3 = 'Image/none.png';
             }
         }
     }else{
-        echo 'error al procesar la petición2';
+        echo 'error al procesar la petición';
         exit;
     }
     
@@ -57,9 +52,6 @@ if($id == '' || $token == ''){
 
 
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
