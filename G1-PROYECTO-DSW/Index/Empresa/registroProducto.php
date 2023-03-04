06f2b845cc8952170e12e3729ac8c79967868005
RenzoTacuri <renzo.tacuri@unmsm.edu.pe>
2023-03-04 18:30:07 -0500

    corrección nombre ventana Emprendimiento

--- G1-PROYECTO-DSW/Index/Empresa/registroProducto.php
+++ G1-PROYECTO-DSW/Index/Empresa/registroProducto.php
@@ -1,16 +1,33 @@
 <?php
+session_start();
+
+if(isset($_SESSION['tipo_usuario'])){
+    if($_SESSION['tipo_usuario']== 'producto'){
+        header("Location: ../Empresa/indexEmpresa.php");
+        exit();
+    }else{
+        header("Location: ../index.php");
+        exit();
+    }
+}
 
 include '../../Conexion/Conexion.php';
+require '../../Clases/Producto.php';
+require '../Components/mensaje.php';
+
+require '../../DAO/ProductoDAO.php';
+
+require 'procesoRegistro.php';
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro Usuario</title>
+    <title>Registro Producto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="../../Estilos/vistaRegistroProduct.css?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
