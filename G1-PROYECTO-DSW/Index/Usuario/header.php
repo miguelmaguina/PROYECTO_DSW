1401e420f7b02fd3170586a2f3d8a9e61f30ed62
LUIS <luis.mayta@unmsm.edu.pe>
2023-03-05 23:26:38 -0500

    buscar solo en tienda1.php

--- G1-PROYECTO-DSW/Index/Usuario/header.php
+++ G1-PROYECTO-DSW/Index/Usuario/header.php
@@ -6,9 +6,13 @@
             <div class="d-flex align-items-center justify-content-between"> 
                 <a href="../index.php" class="logo d-flex align-items-center text-decoration-none"> <img src="../../Image/hallpa.png" alt=""> <span class="titulo-logo d-none d-lg-block">Hallpa</span> </a>
              </div>
-             <div class="search-bar">
+             <?php
+             if(isset($buscarActivado)){
+             echo '<div class="search-bar">
                 <form class="search-form d-flex align-items-center" method="POST" action="#"> <input type="text" name="query" placeholder="Buscar..." title="Escriba"> <button type="submit" title="Buscar"><i class="fa-solid fa-magnifying-glass"></i></button></form>
-             </div>
+             </div>';
+             }
+             ?>
             <button class="navbar-toggler me-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -32,9 +36,13 @@
         <div class="container-fluid col ">
             <nav class="header-nav ms-auto position-absolute" style="right: 20px; top:16px;">
                 <ul class="d-flex align-items-center">
-                    <li class="nav-item d-block d-xl-none p-1">
+                    <?php
+                    if(isset($buscarActivado)){
+                    echo '<li class="nav-item d-block d-xl-none p-1">
                         <a class="nav-link nav-icon search-bar-toggle " href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
-                    </li>
+                    </li>';
+                    }
+                    ?>
                     <li class="nav-item nav-carrito car">
                         <a class="nav-link nav-icon" href="#" ><i class="icono-1 fa-solid fa-cart-shopping"></i></a>
                     </li>
--- G1-PROYECTO-DSW/Index/Usuario/tienda1.php
+++ G1-PROYECTO-DSW/Index/Usuario/tienda1.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$buscarActivado=1; //0 no aparece, 1 aparece
 $tipo=0;//0 no está logueado
 // 1 está logueado como cliente
 // 2 logueado como emprendimiento
