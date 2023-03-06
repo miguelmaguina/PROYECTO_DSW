e2966b16b8f85c445be639efdf4c1a3b01dc2038
IvanSaeco <ivan.saenz@unmsm.edu.pe>
2023-03-05 23:09:52 -0500

    moviendo el footer al footer.php

--- G1-PROYECTO-DSW/Index/Usuario/footer.php
+++ G1-PROYECTO-DSW/Index/Usuario/footer.php
@@ -15,16 +15,16 @@
             </div>
             <div class="box">
                 <h2>SOBRE NOSOTROS</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+                <p>Somos un equipo conformado por estudiantes de la Universidad Nacional Mayor de San Marcos del VII ciclo de la carrera de Ingeniería de Sistemas que han desarrollado esta web e-commerce para ayudar a las comunidades del Perú bajo el marco de las ODS de la Agenda 2030 que promueve la ONU.</p>
+                
             </div>
             <div class="box">
-                <h2>SIGUENOS</h2>
+                <h2>Descubre más acerca de las ODS</h2>
                 <div class="red-social">
-                    <a href="#" class="fa fa-facebook"></a>
-                    <a href="#" class="fa fa-instagram"></a>
-                    <a href="#" class="fa fa-twitter"></a>
-                    <a href="#" class="fa fa-youtube"></a>
+                    <a href="https://www.facebook.com/ONUPeru/" class="fa fa-facebook"></a>
+                    <a href="https://www.instagram.com/onu.peru/?hl=es" class="fa fa-instagram"></a>
+                    <a href="https://twitter.com/ONUPeru" class="fa fa-twitter"></a>
+                    <a href="https://www.youtube.com/watch?v=oAegdV8_7iw" class="fa fa-youtube"></a>
                 </div>
             </div>
         </div>
@@ -32,4 +32,4 @@
             <small>&copy; 2023 <b>Grupo 1</b> - Todos los Derechos Reservados.</small>
             <p>Derechos reservados &copy; 2023 Mi sitio web</p>
         </div>
-    </footer>
+</footer>
--- G1-PROYECTO-DSW/Index/index.php
+++ G1-PROYECTO-DSW/Index/index.php
@@ -405,37 +405,7 @@ if(isset($_SESSION['tipo_usuario'])){
 
     
     <!-- Footer -->
-    
- <footer class="pie-pagina">
-        <div class="grupo-1">
-            <div class="box">
-                <figure>
-                    <a href="#">
-                        <img src="../Image/hallpa.png" alt="Logo de SLee Dw">
-                    </a>
-                </figure>
-            </div>
-            <div class="box">
-                <h2>SOBRE NOSOTROS</h2>
-                <p>Somos un equipo conformado por estudiantes de la Universidad Nacional Mayor de San Marcos del VII ciclo de la carrera de Ingeniería de Sistemas que han desarrollado esta web e-commerce para ayudar a las comunidades del Perú bajo el marco de las ODS de la Agenda 2030 que promueve la ONU.</p>
-                
-            </div>
-            <div class="box">
-                <h2>Descubre más acerca de las ODS</h2>
-                <div class="red-social">
-                    <a href="https://www.facebook.com/ONUPeru/" class="fa fa-facebook"></a>
-                    <a href="https://www.instagram.com/onu.peru/?hl=es" class="fa fa-instagram"></a>
-                    <a href="https://twitter.com/ONUPeru" class="fa fa-twitter"></a>
-                    <a href="https://www.youtube.com/watch?v=oAegdV8_7iw" class="fa fa-youtube"></a>
-                </div>
-            </div>
-        </div>
-        <div class="grupo-2">
-            <small>&copy; 2023 <b>Grupo 1</b> - Todos los Derechos Reservados.</small>
-            <p>Derechos reservados &copy; 2023 Mi sitio web</p>
-        </div>
-    </footer>
-
+    <?php require 'footer.php' ?>
     <script src="../js/index.js?v=<?php echo time(); ?>"></script>
     <script src="../js/carousel.js?v=<?php echo time(); ?>"></script>
     <!-- Scripts de Bootstrap 5 -->
