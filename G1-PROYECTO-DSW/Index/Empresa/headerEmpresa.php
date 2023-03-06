5b5838f6eb42c9e4c5e6e459f32a0e3f81476cfb
LUIS <luis.mayta@unmsm.edu.pe>
2023-03-05 19:49:35 -0500

    modificando ruta de logo en el header

--- G1-PROYECTO-DSW/Index/Empresa/headerEmpresa.php
+++ G1-PROYECTO-DSW/Index/Empresa/headerEmpresa.php
@@ -65,7 +65,7 @@
                       </ul>
                    </li> -->
                    <li class="nav-item dropdown pe-3">
-                            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown"> <img src="../../Image/Emprendimiento/<?php echo $_SESSION['foto_e'];?>" alt="Profile" class="rounded-circle"> <span class="d-none d-md-block dropdown-toggle ps-2" ></span> </a>
+                            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown"> <img src="../../Image/Emprendimientos/<?php echo $_SESSION['foto_e'];?>" alt="Profile" class="rounded-circle"> <span class="d-none d-md-block dropdown-toggle ps-2" ></span> </a>
                             <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                                 <li class="dropdown-header">
                                     <h6><?php echo"".$_SESSION['nombre_e'].""?></h6>
