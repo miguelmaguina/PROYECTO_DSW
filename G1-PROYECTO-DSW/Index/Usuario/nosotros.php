9523b63e6b4b0a2a91d12f627718f60c6c6d9c58
miguel.maguina <miguel.g3n10@gmail.com>
2023-03-06 22:57:27 -0500

    Actualización de información

--- G1-PROYECTO-DSW/Index/Usuario/nosotros.php
+++ G1-PROYECTO-DSW/Index/Usuario/nosotros.php
@@ -20,7 +20,7 @@ if(isset($_SESSION['tipo_usuario'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Nostros</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="../../Estilos/styleNosotros.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../../Estilos/header.css?v=<?php echo time(); ?>">
@@ -42,8 +42,8 @@ if(isset($_SESSION['tipo_usuario'])){
 <div class="portada1 container-fluid py-1 d-flex align-items-center">
         <div class="row">
             <div class="contenedor col-lg-9 mx-auto text-center">
-                <p class="title-1 fs-2 h3" style="color: white;">Productos elaborados</p><p class="title-2 fs-1 h2" style="color: #035941;">por comunidades nativas del Perú</p>
-                <p class="descripcion">Déjese sorprender por la belleza y autenticidad de los productos hechos a mano por las comunidades nativas peruanas. Desde ropa tejida con técnicas tradicionales hasta joyas y objetos de decoración únicos, cada uno de nuestros productos cuenta con una historia y una cultura detrás. Al comprar en nuestro e-commerce, no solo estará llevándose a casa un objeto hermoso, sino que también estará apoyando a las comunidades nativas y preservando sus tradiciones. ¡Descubra la belleza de las comunidades nativas peruanas hoy mismo!</p>
+                <p class="title-1 fs-2 h3" style="color: white;">Productos elaborados</p><p class="title-2 fs-1 h2" style="color: #93CF4F;">por comunidades campesinas y nativas del Perú</p>
+                <p class="descripcion">Déjese sorprender por la belleza y autenticidad de los productos hechos a mano por las comunidades peruanas. Desde ropa tejida con técnicas tradicionales hasta joyas y objetos de decoración únicos, cada uno de nuestros productos cuenta con una historia y una cultura detrás. Al comprar en nuestro e-commerce, no solo estará llevándose a casa un objeto hermoso, sino que también estará apoyando a las comunidades y preservando sus tradiciones. ¡Descubra la belleza de las comunidades peruanas hoy mismo!</p>
                 <div class="btn-group">
                     <?php if($tipo==0){
                         echo '<a href="tienda1.php" class="btn btn-izq btn-block mx-4 rounded fw-bold">Ir a la tienda virtual</a>
@@ -67,7 +67,7 @@ if(isset($_SESSION['tipo_usuario'])){
                 </div>
                 <div class="col-md-6 p-5">
                   <h2 >¿Qué somos?</h2>
-                  <p class="texto">Somos un e-commerce dedicado a conectar a los consumidores con las comunidades nativas peruanas. Creemos en la importancia de preservar las tradiciones y culturas locales, y queremos compartir la riqueza de estas comunidades con el mundo. Todos nuestros productos son hechos a mano por artesanos locales y están cuidadosamente seleccionados para ofrecer solo lo mejor. Al comprar en nuestro e-commerce, no solo estará obteniendo productos únicos y hermosos, sino que también estará apoyando directamente a las comunidades nativas y a sus tradiciones. </p><p class="texto">¡Bienvenido a nuestro mundo auténtico de comunidades nativas peruanas!.</p>
+                  <p class="texto">Somos un e-commerce dedicado a conectar a los consumidores con las comunidades campesinas y nativas peruanas. Creemos en la importancia de preservar las tradiciones y culturas locales, y queremos compartir la riqueza de estas comunidades con el mundo. Todos nuestros productos son hechos a mano por artesanos locales y están cuidadosamente seleccionados para ofrecer solo lo mejor. Al comprar en nuestro e-commerce, no solo estará obteniendo productos únicos y hermosos, sino que también estará apoyando directamente a las comunidades y a sus tradiciones. </p><p class="texto">¡Bienvenido a nuestro mundo auténtico de comunidades nativas peruanas!.</p>
                 </div>
             </div>
         </div>
@@ -78,8 +78,8 @@ if(isset($_SESSION['tipo_usuario'])){
             <div class="row row-p1">
                 <div class="col-md-6 p-5">
                   <h2>Visión y Misión</h2>
-                  <p class="texto">Nuestra misión es conectar a los consumidores con las comunidades nativas peruanas y preservar sus tradiciones a través de nuestro e-commerce. Creemos en la importancia de apoyar directamente a las comunidades locales y compartir su riqueza cultural con el mundo.
-Nuestra visión es ser líderes en la promoción de productos auténticos de las comunidades nativas peruanas y ser reconocidos como una fuente confiable y respetada de artesanías únicas.</p>
+                  <p class="texto">Nuestra misión es conectar a los consumidores con las comunidades del Perú y preservar sus tradiciones a través de nuestro e-commerce. Creemos en la importancia de apoyar directamente a las comunidades locales y compartir su riqueza cultural con el mundo.
+<br><br>Nuestra visión es ser líderes en la promoción de productos auténticos de las comunidades peruanas y ser reconocidos como una fuente confiable y respetada de artesanías únicas.</p>
                 </div>
 
                 <div class="col-md-6 content-img">
